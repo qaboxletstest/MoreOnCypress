1c906bf6475ca69fb1cdc0643026adf3382ef393
qaboxletstest <qaboxletstest@gmail.com>
2021-07-02 12:06:21 +0530

    Delete Videos for Passed Test Cases

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -7,5 +7,17 @@ module.exports = (on, config) => {
       return del(results.video)
     }
   })
+  on('after:spec', (spec, results) => {
+    if (results && results.video) {
+      // Do we have failures for any retry attempts?
+      const failures = _.some(results.tests, (test) => {
+        return _.some(test.attempts, { state: 'failed' })
+      })
+      if (!failures) {
+        // delete the video if the spec passed and no tests retried
+        return del(results.video)
+      }
+    }
+  })
   return config;
 }
