6be4083adf78bf7b197cb9b0f3a4d51e0c06eb7e
qaboxletstest <qaboxletstest@gmail.com>
2021-08-03 18:36:31 +0530

    Load Extension

--- cypress/integration/LoadingBar.spec.js
+++ cypress/integration/LoadingBar.spec.js
@@ -1,9 +1,9 @@
 describe('Testing the loading bar', () => {
     it('Check loading bar appears', () => {
         cy.visit("http://localhost:3000");
-        cy.intercept("/api/users?page=2", (req) => {
-            return Cypress.Promise.delay(5000).then(() => req.continue())
-        }).as("users")
+        // cy.intercept("/api/users?page=2", (req) => {
+        //     return Cypress.Promise.delay(5000).then(() => req.continue())
+        // }).as("users")
         cy.get("#load").click()
         cy.get("#loader").should("be.visible")
         cy.get("#loader").should("not.exist")
