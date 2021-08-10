1561c44a7cc5605d34b155c84800472157dc598e
qaboxletstest <qaboxletstest@gmail.com>
2021-08-10 13:54:09 +0530

    Intercep GraphQL API Call

--- cypress/integration/Intercept_GraphQL.spec.js
+++ cypress/integration/Intercept_GraphQL.spec.js
@@ -1,10 +1,17 @@
 describe('Intercept GraphQL Request', () => {
-    it('Check ToDo is added', () => {
-        const URL = "https://graphql-java-spring-boot.herokuapp.com/graphql"
+    it.skip('Check ToDo is added - OLD SCHOOL WAY', () => {
         cy.visit("https://graphql-to-do-app-angular.herokuapp.com/")
         cy.contains("div.create-item-title", "Create New Item").click()
-        cy.get('input[placeholder="Item Name"]').type("Intercept GraphQL Request")
+        cy.get('input[placeholder="Item Name"]').type("OLD SCHOOL")
+        cy.get('input[value="Create Item"]').click()
+        cy.contains("div#1 div.item-wrapper>div", "OLD SCHOOL", { timeout: 10000 })
+            .should("be.visible")
 
+    });
+
+    it.only("Check ToDo is added - Yeah Baby...it's Cypress", () => {
+        const URL = "https://graphql-java-spring-boot.herokuapp.com/graphql"
+        cy.visit("https://graphql-to-do-app-angular.herokuapp.com/")
         // Intercept - Create Item
         cy.intercept("POST", URL, (req) => {
             if (req.body.operationName === "createItemMutation") {
@@ -15,13 +22,16 @@ describe('Intercept GraphQL Request', () => {
         // Intercept - Show on HomePage
         cy.intercept("POST", URL, (req) => {
             if (req.body.operationName === "homePageQuery") {
-                req.alias = "HomePage"
+                req.alias = "RenderItem"
                 req.continue()
             }
         })
+        cy.contains("div.create-item-title", "Create New Item").click()
+        cy.get('input[placeholder="Item Name"]').type("Intercept GraphQL Request")
         cy.get('input[value="Create Item"]').click()
+        // Wait for both the requests to be completed
         cy.wait("@CreateItem")
-        cy.wait("@HomePage")
+        cy.wait("@RenderItem")
         cy.contains("div#1 div.item-wrapper>div", "Intercept GraphQL Request")
             .should("be.visible")
 
