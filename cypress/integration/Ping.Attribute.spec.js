49b68eb4da102c13ebf9ba9e046eb3d70553dafd
qaboxletstest <qaboxletstest@gmail.com>
2021-08-12 15:09:42 +0530

    Ping Attribute Testing

--- cypress/integration/Ping.Attribute.spec.js
+++ cypress/integration/Ping.Attribute.spec.js
@@ -1,12 +1,14 @@
 describe('Anchor Tag - ping attribute ', () => {
     it('ping attribute testing', () => {
         cy.visit("http://127.0.0.1:5500/Other/pingAttr.html")
+        // INTERCEPT
         cy.intercept({
             "method": "POST",
-            "url": "http://localhost:3000/ping?q=1000"
+            "url": "http://localhost:3000/ping?q=900"
         }).as("ping")
         cy.get("a#anchor").click()
         cy.location("pathname").should("equal", "/Other/shadowDom.html")
+        // WAIT AND ASSERT PING-TO and PING-FROM REQUEST HEADERS
         cy.wait("@ping").its("request.headers").should("deep.include", {
             "ping-from": "http://127.0.0.1:5500/Other/pingAttr.html",
             "ping-to": "http://127.0.0.1:5500/Other/shadowDom.html"
