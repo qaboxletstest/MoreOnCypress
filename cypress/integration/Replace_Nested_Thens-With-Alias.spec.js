74550cc607ecd6b0a36cf474131a8ef2fda5c98b
qaboxletstest <qaboxletstest@gmail.com>
2021-08-17 19:08:34 +0530

    Replace Nested Thens with Alias

---
+++ cypress/integration/Replace_Nested_Thens-With-Alias.spec.js
@@ -0,0 +1,36 @@
+describe('Replace Nested Thens with Alias', () => {
+
+    beforeEach(() => {
+        cy.intercept("http://localhost:4000/users?_start=0&_end=4").as("initial")
+        cy.visit("http://localhost:3000/")
+        cy.wait("@initial")
+    })
+
+    it.skip('Nested Then', () => {
+        cy.get("span[ref='lbRecordCount']").then($el1 => {
+            cy.get("span[ref='lbLastRowOnPage']").then($el2 => {
+                cy.get("span[ref='lbFirstRowOnPage']").then($el3 => {
+                    const totalRows = parseInt($el1.text())
+                    const totalRowsUB = parseInt($el2.text())
+                    const totalRowsLB = parseInt($el3.text())
+                    const iterCount = (totalRows) / (totalRowsUB - totalRowsLB - 1)
+                    for (let index = 0; index < iterCount; index++) {
+                        cy.contains("button.ag-paging-button", "Next").click()
+                    }
+                })
+            })
+        })
+    });
+
+    it.skip('Alias', () => {
+        cy.get("span[ref='lbRecordCount']").invoke('text').then(parseInt).as('totalRows')
+        cy.get("span[ref='lbLastRowOnPage']").invoke('text').then(parseInt).as('totalRowsUB')
+        cy.get("span[ref='lbFirstRowOnPage']").invoke('text').then(parseInt).as('totalRowsLB')
+            .then(function () {
+                const iterCount = (this.totalRows) / (this.totalRowsUB - this.totalRowsLB - 1)
+                for (let index = 0; index < iterCount; index++) {
+                    cy.contains("button.ag-paging-button", "Next").click()
+                }
+            })
+    });
+});
